23ade5043fe01333b725e552a2ba4b756946abae
John Griffith <john.griffith@solidfire.com>
2012-07-16 09:12:49 -0600

    add checks for vagrant setup, somebody I'll right a recipee but this works for now

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-
 cp -R .vim ~/
 cp .vimrc ~/
 cp .gitconfig ~/
 sudo apt-get install pyflakes
+if [ -d /home/vagrant ]; then
+    cp .gitconfig ~/
+    sudo apt-get install pyflakes
+    ln -s /home/vagrant/.host-ssh/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
+    sudo pip install git-review
+fi
+
