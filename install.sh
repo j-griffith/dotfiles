5d862f6cfae54b415388dd13ef9ca91699c35a65
vagrant <vagrant@precise64>
2012-11-29 16:22:28 +0000

    Always install pyflakes and git-review

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@ cp -R .vim ~/
 cp .vimrc ~/
 cp .gitconfig ~/
 sudo apt-get install pyflakes
+sudo pip install git-review
 if [ -d /home/vagrant ]; then
     cp .gitconfig ~/
-    sudo apt-get install pyflakes
     ln -s /home/vagrant/.host-ssh/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
-    sudo pip install git-review
 fi
 
